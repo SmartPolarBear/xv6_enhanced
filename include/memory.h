c54fa8e9dd93c07bc1a6a2e66e1ce846b811f97a
SmartPolarBear <clevercoolbear@outlook.com>
2022-10-19 22:42:44 +0800

    feat(net): add lwip code from CSEP 551 skeleton c ode

--- include/memory.h
+++ include/memory.h
@@ -8,13 +8,7 @@
 
 enum gfp_flags
 {
-	GFP_KERNEL = 0x01,
-	GFP_ATOMIC = 0x02,
-	GFP_NOIO = 0x04,
-	GFP_NOFS = 0x08,
-	GFP_NOWAIT = 0x10,
-	GFP_NOFAIL = 0x20,
-	GFP_NORETRY = 0x40,
+	GFP_DMA = 0x1
 };
 
 void *kmalloc(size_t size, gfp_t flags);
